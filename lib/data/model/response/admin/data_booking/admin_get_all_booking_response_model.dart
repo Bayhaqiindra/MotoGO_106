59acef2b14ae54174ea3cfe1585e6dc50595a00d
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-07-13 02:51:59 +0700

    membuat model data booking admin

---
+++ lib/data/model/response/admin/data_booking/admin_get_all_booking_response_model.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+
+import 'package:tugas_akhir/data/model/response/pelanggan/pelanggan_response_model.dart';
+
+class AdmingetallbookingResponseModel {
+    final int? bookingId;
+    final int? idPelanggan;
+    final int? serviceId;
+    final String? status;
+    final String? customerNotes;
+    final String? pickupLocation;
+    final DateTime? createdAt;
+    final DateTime? updatedAt;
+    final String? latitude;
+    final String? longitude;
+    final Pelanggan? pelanggan;
+
+    AdmingetallbookingResponseModel({
+        this.bookingId,
+        this.idPelanggan,
+        this.serviceId,
+        this.status,
+        this.customerNotes,
+        this.pickupLocation,
+        this.createdAt,
+        this.updatedAt,
+        this.latitude,
+        this.longitude,
+        this.pelanggan,
+    });
+
+    factory AdmingetallbookingResponseModel.fromJson(String str) => AdmingetallbookingResponseModel.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory AdmingetallbookingResponseModel.fromMap(Map<String, dynamic> json) => AdmingetallbookingResponseModel(
+        bookingId: json["booking_id"],
+        idPelanggan: json["id_pelanggan"],
+        serviceId: json["service_id"],
+        status: json["status"],
+        customerNotes: json["customer_notes"],
+        pickupLocation: json["pickup_location"],
+        createdAt: json["created_at"] == null ? null : DateTime.parse(json["created_at"]),
+        updatedAt: json["updated_at"] == null ? null : DateTime.parse(json["updated_at"]),
+        latitude: json["latitude"],
+        longitude: json["longitude"],
+        pelanggan: json["pelanggan"] == null ? null : Pelanggan.fromMap(json["pelanggan"]),
+    );
+
+    Map<String, dynamic> toMap() => {
+        "booking_id": bookingId,
+        "id_pelanggan": idPelanggan,
+        "service_id": serviceId,
+        "status": status,
+        "customer_notes": customerNotes,
+        "pickup_location": pickupLocation,
+        "created_at": createdAt?.toIso8601String(),
+        "updated_at": updatedAt?.toIso8601String(),
+        "latitude": latitude,
+        "longitude": longitude,
+        "pelanggan": pelanggan?.toMap(),
+    };
+}
